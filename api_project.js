90269e132f987ecc6848da9e503cbd3434a2dcd8
Brian Horakh <brian.horakh@cloudbeds.com>
2014-09-26 17:07:39 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,6 +6,6 @@ define({
   "url": "https://api.myallocator.com/pms/v201408/{json|xml}",
   "generator": {
     "version": "0.7.1",
-    "time": "2014-09-26T16:57:33.699Z"
+    "time": "2014-09-26T17:07:39.890Z"
   }
 });
