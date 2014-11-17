5a0db397be625013f74c59030892d2bb47c797e8
Shahin <brian.horakh@cloudbeds.com>
2014-11-17 19:18:03 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,6 +6,6 @@ define({
   "url": "https://api.myallocator.com/pms/v201408/{json|xml}",
   "generator": {
     "version": "0.7.2",
-    "time": "2014-11-11T20:39:45.976Z"
+    "time": "2014-11-17T19:18:03.240Z"
   }
 });
