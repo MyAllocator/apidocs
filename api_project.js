017d30a7150ac5ea2ed6147b877c61d438ff88a2
Shahin <brian.horakh@cloudbeds.com>
2014-12-05 06:40:43 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -4,7 +4,7 @@ define({
   "description": "MyAllocator new and improved API",
   "generator": {
     "version": "0.9.0",
-    "time": "2014-12-05T06:39:57.576Z"
+    "time": "2014-12-05T06:40:43.192Z"
   },
   "apidoc": "0.2.0"
 });
