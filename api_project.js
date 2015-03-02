abd7362af02a7a15ef044215008003aca58b577c
Shahin <shahinul.islam@cloudbeds.com>
2015-03-02 18:02:11 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-03-02T17:32:41.517Z",
+    "time": "2015-03-02T18:02:11.442Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
