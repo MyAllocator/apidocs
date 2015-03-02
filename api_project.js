1534382eb1b6dc9575c786e3d3dce3df0890df0d
Shahin <brian.horakh@cloudbeds.com>
2015-03-02 16:22:56 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-03-02T15:26:07.410Z",
+    "time": "2015-03-02T16:22:56.016Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
