724123dd18896949b5fd24847de3898f074abe89
Brian Horakh <brian.horakh@cloudbeds.com>
2015-05-20 23:16:19 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-05-11T15:39:02.775Z",
+    "time": "2015-05-20T23:16:19.114Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
