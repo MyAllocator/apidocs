c15123b79d4a8941e8ab07a8fe66658e8626606a
nathanhelenihi <nathan.helenihi@cloudbeds.com>
2016-05-31 19:52:30 +0000

    doc release

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-05-30T18:39:23.835Z",
+    "time": "2016-05-31T19:52:30.121Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
