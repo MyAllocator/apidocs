c0e27d66954e5ec4db691b79e2773f4f93aa35e0
mo <mo@myallocator.com>
2017-05-08 19:58:04 +0200

    doc release

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T17:52:32.949Z",
+    "time": "2017-05-08T17:58:04.385Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
