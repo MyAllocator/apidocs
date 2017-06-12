0e0f47e37f668b26499439ad44ec754791269bda
mo <mo@myallocator.com>
2017-06-12 17:43:38 +0200

    doc release

--- api_project.js
+++ api_project.js
@@ -3,12 +3,12 @@ define({
   "version": "201610.0.2",
   "description": "myallocator API for PMS partners.",
   "title": "myallocator PMS API",
-  "url": "https://myallocator.github.io/apidocs",
+  "url": "https://api.myallocator.com",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-12T15:41:19.311Z",
+    "time": "2017-06-12T15:43:38.107Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
