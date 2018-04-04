462d17b8cf3b708c8dec8234212f47afce408276
mo <mo@myallocator.com>
2018-04-04 13:08:43 +0100

    doc release

--- api_project.js
+++ api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "myallocator PMS API",
-  "version": "201610.0.2",
+  "version": "201804.0.1",
   "description": "myallocator API for PMS partners.",
   "title": "myallocator PMS API",
   "url": "https://api.myallocator.com/pms/v<VERSION>/<json|xml>",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-04-04T12:07:22.861Z",
+    "time": "2018-04-04T12:08:42.819Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
