137ecbdee6c109bc933edc551406478a9bb8bb3f
mo <mo@myallocator.com>
2018-11-26 13:22:10 +0000

    updated apidocs

--- api_project.js
+++ api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-08-20T11:33:18.511Z",
+    "time": "2018-11-26T13:21:09.779Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.6"
+    "version": "0.2.0"
   }
 });
